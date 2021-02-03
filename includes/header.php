04466b1cd3e9ba55135779c63d84737d63375d89
Nils <vosnils18@icloud.com>
2021-02-03 13:55:36 +0100

    header (still no styling)

--- includes/header.php
+++ includes/header.php
@@ -9,10 +9,10 @@ session_start();
 </title>
 <header>
     <div class="menubar">
-        <img src="../images/logo_V2_large.png" alt="../images/logo_V2.svg">
+        <a href="./index.php"><img src="./images/logo_V2_large.png" alt="./images/logo_V2.svg"></a>
         <div class="topnav">
-            <a href="../profile.php">profile</a>
-            <a href="../cart.php"><img src="../images/icons8-shopping-cart-100.png"></a>
+            <a href="./profile.php"><img src="./images/icons8-customer-96.png"></a>
+            <a href="./cart.php"><img src="./images/icons8-shopping-cart-100.png"></a>
         </div>
     </div>
 </header>
