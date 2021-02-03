19121f2a7fc744e95638c84bb0a955100bc98519
Nils <vosnils18@icloud.com>
2021-02-03 13:47:30 +0100

    header (no styling)

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="eng">
+<title>
+    Lyfters
+</title>
+<header>
+    <div class="menubar">
+        <img src="../images/logo_V2_large.png" alt="../images/logo_V2.svg">
+        <div class="topnav">
+            <a href="../profile.php">profile</a>
+            <a href="../cart.php"><img src="../images/icons8-shopping-cart-100.png"></a>
+        </div>
+    </div>
+</header>
+</html>
