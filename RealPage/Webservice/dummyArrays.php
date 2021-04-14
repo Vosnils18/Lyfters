fed06ed17bf8b583a5865d42560f40de9a98f275
Nils <vosnils18@icloud.com>
2021-04-14 13:25:17 +0200

    meest recente versie van lyfters

--- RealPage/Webservice/dummyArrays.php
+++ RealPage/Webservice/dummyArrays.php
@@ -4,24 +4,24 @@ function getImageSlideshow() {
 
     return [
         [
-            'id' => 1,
-            'image' => 'https://via.placeholder.com/468x150?text=468x150+Full+Banner+1',
-            'caption' => 'Schoenen zijn best cool soms'
+            'slideshowId' => 1,
+            'slideshowImage' => 'https://via.placeholder.com/468x150?text=468x150+Full+Banner+1',
+            'slideshowCaption' => 'Schoenen zijn best cool soms'
         ],
         [
-            'id' => 2,
-            'image' => 'https://via.placeholder.com/468x150?text=468x150+Full+Banner+2',
-            'caption' => 'Schoenen zijn best cool soms'
+            'slideshowId' => 2,
+            'slideshowImage' => 'https://via.placeholder.com/468x150?text=468x150+Full+Banner+2',
+            'slideshowCaption' => 'Schoenen zijn best cool soms'
         ],
         [
-            'id' => 3,
-            'image' => 'https://via.placeholder.com/468x150?text=468x150+Full+Banner+3',
-            'caption' => 'Schoenen zijn best cool soms'
+            'slideshowId' => 3,
+            'slideshowImage' => 'https://via.placeholder.com/468x150?text=468x150+Full+Banner+3',
+            'slideshowCaption' => 'Schoenen zijn best cool soms'
         ],
         [
-            'id' => 4,
-            'image' => 'https://via.placeholder.com/468x150?text=468x150+Full+Banner+4',
-            'caption' => 'Schoenen zijn best cool soms'
+            'slideshowId' => 4,
+            'slideshowImage' => 'https://via.placeholder.com/468x150?text=468x150+Full+Banner+4',
+            'slideshowCaption' => 'Schoenen zijn best cool soms'
         ],
     ];
 }
@@ -32,16 +32,18 @@ function getProductDetails() {
         [
             'id' => 1,
             'name' => 'Midnight Black',
-            'image' => 'https://via.placeholder.com/400x400?text=Product+Image+Black',
-            'colour' => '#000000',
-            'available_sizes' => ['35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45']
+            'images' => ['https://via.placeholder.com/400x400?text=Product+Image+Black1', 'https://via.placeholder.com/400x400?text=Product+Image+Black2'],
+            'color' => 'Black',
+            'available_sizes' => ['35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45'],
+            'description' => 'lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.'
         ],
         [
             'id' => 2,
             'name' => 'Shining White',
-            'image' => 'https://via.placeholder.com/400x400?text=Product+Image+White',
-            'colour' => '#FFFFFF',
-            'available_sizes' => ['35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45']
+            'images' => ['https://via.placeholder.com/400x400?text=Product+Image+White1', 'https://via.placeholder.com/400x400?text=Product+Image+White2'],
+            'color' => 'White',
+            'available_sizes' => ['35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45'],
+            'description' => 'lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.'
         ],
     ];
 }
