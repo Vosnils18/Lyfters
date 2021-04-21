e3394d2bed175ca50ae6ebec6bb464f96069058c
Nils <vosnils18@icloud.com>
2021-04-21 21:37:01 +0200

    Php gefixt?

--- Landingpage/signup.php
+++ Landingpage/signup.php
@@ -1,19 +1,32 @@
 <?php
 
-$host="localhost";
-$username="root";
-$password="";
-$databasename="sample";
+$serverName="db.lyfters-shop.com";
+$dBUsername="md567856db557169";
+$dBPassword="Klaasfaak@21";
+$dBName="md567856db557169";
 
-$connect=mysql_connect($host,$username,$password);
-$db=mysql_select_db($databasename);
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
-if(isset($_POST['submit_form']))
-{
- $name=$_POST["user_name"];
- $email=$_POST["email"];
+$name = $_POST['user_name'];
+$email = $_POST['email'];
 
- mysql_query("insert into short_urls values('','$name','$email')");
- echo "Thank You For Joining Our Newsletter";
+if (!$conn) {
+ die("Connection failed: " . mysqli_connect_error());
 }
-?>
+
+
+$sql = "INSERT INTO newsletter (name, email) VALUES(?, ?);";
+$stmt = mysqli_stmt_init($conn);
+if (!mysqli_stmt_prepare($stmt, $sql)) {
+    header('location: ./index.html?error=stmtfailed');
+    exit();
+}
+
+
+mysqli_stmt_bind_param($stmt, 'ss', $name, $email);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_close($stmt);
+
+header('location: ./index.html?error=none');
+mysqli_field_count($stmt);
+exit();
